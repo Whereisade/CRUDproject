e8f4b75351cd59913235b2d84e4ed3d35a935bb9
Whereisade <eniolaadeleke2303@gmail.com>
2024-12-21 17:24:30 +0100

    FEAT: implemented the admin

--- AppTwo/admin.py
+++ AppTwo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_of_birth')  
+    search_fields = ('name',)  
+    list_filter = ('date_of_birth',) 
