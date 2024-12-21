bc0caf318ba79d88e79d4034709db6cb34a12ac6
Whereisade <eniolaadeleke2303@gmail.com>
2024-12-21 13:03:33 +0100

    FEAT: Fixed the author serializer

--- AppTwo/models.py
+++ AppTwo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return self.name
