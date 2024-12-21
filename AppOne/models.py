cf48308a4417e6306784986a5389d6d6e3700e70
Whereisade <eniolaadeleke2303@gmail.com>
2024-12-21 18:15:51 +0100

    FEAT: Added new authors

--- AppOne/models.py
+++ AppOne/models.py
@@ -4,10 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_date = models.DateField()
-    isbn = models.CharField(max_length=13, unique=True)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
